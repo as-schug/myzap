c332411457cc20c902302b820314cc3c2b59a2b3
Antonio S Schug <antoniosidineischug@gmail.com>
2022-09-21 09:41:55 -0300

    Update WppConnect.js
    Restore connection correction

--- engines/WppConnect.js
+++ engines/WppConnect.js
@@ -84,6 +84,8 @@ export default class Wppconnect {
                     '--disable-app-list-dismiss-on-blur',
                     '--disable-accelerated-video-decode',
                 ],
+   		        
+				puppeteerOptions: { userDataDir: './tokens/' + session, },
 
                 createPathFileToken: false,
                 sessionToken: {
