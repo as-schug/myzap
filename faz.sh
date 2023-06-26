504ff3f5c8e3b2ed27bc8f542c9ed6569f19049b
root <root@myNode>
2023-06-26 09:49:35 -0300

    Adições

---
+++ faz.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+
+while [  true  ]
+do
+  echo Starting...
+  chmod 777 tokens
+  npm start
+done
+      
+      
---
+++ run.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+
+#docker build  -t myzap .
+
+#docker run --name myzap -p 3333:3333 myzap
+docker run -d --name myzap -p 3334:3333 -p 3333:3333 --rm  myzap
+#docker run --name myzap -v /mnt/hdd/tokens:/usr/src/app/tokens  -p 3334:3333 -p 3333:3333 --rm  myzap
+docker start myzap
+
+
