855773a1f1135569c82697adfbfade8766bf2b7f
asschug <antoniosidineischug@gmail.com>
2023-06-28 06:47:55 -0300

    changes

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@
 #docker build  -t myzap .
 
 #docker run --name myzap -p 3333:3333 myzap
-docker run -d --name myzap -p 3334:3333 -p 3333:3333 -v /mnt/hdd/tokens:/usr/src/app/tokens myzap
+docker start myzap || \
+   docker run -d --name myzap -p 3334:3333 -p 3333:3333 -v /mnt/hdd/tokens:/usr/src/app/tokens myzap
 #docker run --name myzap -v /mnt/hdd/tokens:/usr/src/app/tokens  -p 3334:3333 -p 3333:3333 --rm  myzap
 docker start myzap
 
