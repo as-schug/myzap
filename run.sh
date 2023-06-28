a25afd86111c063216b1079d613004cd3055913c
as-schug <antoniosidineischug@gmail.com>
2023-06-28 15:24:55 -0300

    run.sh

--- run.sh
+++ run.sh
@@ -7,4 +7,5 @@ docker start myzap || \
    docker run -d --name myzap -p 3334:3333 -p 3333:3333 -v /mnt/hdd/tokens:/usr/src/app/tokens myzap
 
 #watch "ps -efww|grep pidof -s node"
+#ps -efw|grep user-data-dir
 
