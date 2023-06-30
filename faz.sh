b4c7b20c1670f380611e257098b40b8314f74cc9
as-schug <antoniosidineischug@gmail.com>
2023-06-30 19:17:39 -0300

    changes

--- faz.sh
+++ faz.sh
@@ -4,6 +4,10 @@
 chmod 777 webhook
 chmod 777 webhook/*
 
+find webhook -type f -ctime +10 -exec chmod 666 {} \; -print
+find webhook -empty -delete -ctime +10 -print 
+
+
 while [  true  ]
 do
   echo Starting...
