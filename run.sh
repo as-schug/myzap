8c3df693bdebece650fabff5347cab5c9008271b
Antonio S Schug <antoniosidineischug@gmail.com>
2024-06-15 02:03:39 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,27 @@
 #! /bin/bash
 
+MODULO=myzap
+if [ -f /etc/terasoft.conf ]
+then
+  . /etc/terasoft.conf
+fi
+
+if [ -z "$RAIZ" ]
+then
+   echo definir a variavel RAIZ
+   exit 1
+fi
+
+if [ -z "$*" ]
+then
+   ARGS=-d
+fi
+
+
 
 #docker build  -t myzap .
 
-/usr/bin/docker start myzap || \
-   /usr/bin/docker run -d --name myzap -p 3334:3333 -p 3333:3333 -v /mnt/hdd/tokens:/usr/src/app/tokens myzap
+/usr/bin/docker start $MODULO || \
+   /usr/bin/docker run -d --name $MODULO -p 3334:3333 -p 3333:3333 -v $RAIZ/data/$MODULO/tokens:/usr/src/app/tokens myzap
 
-#watch "ps -efww|grep pidof -s node"
-#ps -efw|grep user-data-dir
 
